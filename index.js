54c9df821ca14d4a3204456a557d12f7347dafa5
ednihilator <boyofpopsicle@gmail.com>
2023-07-23 16:39:18 -0400

    install the inquirer package

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require("inquirer");
+const shapes = require("./lib/shapes.js");
+const fs = require("fs");
+
+const questions = [
+  {
+    type: "input",
+    message: "Text up to 3 characters",
+    name: "text",
+  },
+  {
+    type: "input",
+    message: "text color",
+    name: "text_color",
+  },
+  {
+    type: "list",
+    message: "shape",
+    name: "shape",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    message: "Shape's color",
+    name: "shape_color",
+  },
+];
+
+inquirer.prompt(questions).then((response) => {
+  const logoGen = genLogo(response);
+  writeToFile("./genLogo/logo.svg", logoGen);
+});
+
+//function creates the logo.svg file
+function writeToFile(fileName, data) {
+  fs.writeToFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("logo.svg made!")
+  );
+}
+
+const genLogo = {
+  text,
+  text_color,
+  shape,
+  shape_color,
+};
