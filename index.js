8d6e6ea159c2a16a1d8ab956353130c05cede42f
ednihilator <boyofpopsicle@gmail.com>
2023-07-26 17:45:28 -0400

    added .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const shapes = require("./lib/shapes.js");
+const { Circle, Square, Triangle } = require("./lib/shapes.js");
 const fs = require("fs");
 
 const questions = [
@@ -25,17 +25,45 @@ const questions = [
     name: "shape_color",
   },
 ];
+async function generateLogo() {
+  const { shape, text, text_color, shape_color } = await inquirer.prompt(
+    questions
+  );
 
-inquirer.prompt(questions).then((response) => {
-  const logoGen = genLogo(response);
-  writeToFile("./genLogo/logo.svg", logoGen);
-});
+  let logo;
+  switch (shape) {
+    case "Circle":
+      logo = new Circle(text, text_color, shape_color);
+      break;
+    case "Square":
+      logo = new Square(text, text_color, shape_color);
+      break;
+    case "Triangle":
+      logo = new Triangle(text, text_color, shape_color);
+      break;
+  }
+  writeFile("./genLogo/logo.svg", logo.genLogo());
+}
+generateLogo();
+// inquirer.prompt(questions).then(({ shape, text, text_color, shape_color }) => {
+//   let logo;
+//   switch (shape) {
+//     case "Circle":
+//       logo = new Circle(text, text_color, shape_color);
+//       break;
+//     case "Square":
+//       logo = new Square(text, text_color, shape_color);
+//       break;
+//     case "Triangle":
+//       logo = new Triangle(text, text_color, shape_color);
+//       break;
+//   }
 
 //function creates the logo.svg file
-function writeToFile(fileName, data) {
-  fs.writeToFile(fileName, data, (err) =>
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log("logo.svg made!")
   );
 }
 
-function genLogo(text, text_color, shape, shape_color) {}
+// function genLogo(text, text_color, shape, shape_color) {}
