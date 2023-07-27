5acd3eb1f2a6bf2a936c207d54b6ff4fd147f1d7
ednihilator <boyofpopsicle@gmail.com>
2023-07-27 18:34:14 -0400

    oops i forgot to enter more commits. this is done.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Logo Generator
+
+## Description
+
+This project generates a logo based off of user inquirers. This will help users make rough drafts of potential logo ideas.
+
+## Table of Contents
+
+[credits](#credits)
+
+[license](#license)
+
+[questions](#questions)
+
+[contribution](#contributions)
+
+## Installation
+
+Download the module 10 folder, run npm i.
+
+## Usage
+
+Type node server.js and answer the questions.
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+N/A
+
+## License
+
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+        This project is covered under the MIT license.
+
+## Tests
+
+run npm test in the terminal
+
+## Contributions
+
+N/A
+
+## Questions
+
+Email: boyofpopsicle@gmail.com
+
+Github Profile Link: [ednihilator](https://www.github.com/ednihilator)
--- index.js
+++ index.js
@@ -25,6 +25,41 @@ const questions = [
     name: "shape_color",
   },
 ];
+
+// class SVG {
+//   constructor(text, text_color, shape, shape_color) {
+//     this.text = text;
+//     this.shape = shape;
+//     this.shape_color = shape_color;
+//     this.text_color = text_color;
+//     this.shapeInfo = "";
+//     this.textInfo = "";
+//   }
+
+//   genLogo() {
+//     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+//   }
+
+// insertTextInfo(text, text_color) {
+//   this.textInfo = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${text_color}">${text}</text>`;
+// }
+// insertShapeInfo(shape) {
+//   this.shapeInfo = shape.genLogo();
+// }
+// genLogo() {
+//     return `<svg version="1.1"
+//      width="300" height="200"
+//      xmlns="http://www.w3.org/2000/svg">
+
+//   <circle cx="150" cy="100" r="80" fill="${this.shape_color}" />
+
+//   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.text_color}">${this.text}</text>
+
+// </svg>`;
+//   }
+// }
+// }
+
 async function generateLogo() {
   const { shape, text, text_color, shape_color } = await inquirer.prompt(
     questions
@@ -45,25 +80,9 @@ async function generateLogo() {
   writeFile("./genLogo/logo.svg", logo.genLogo());
 }
 generateLogo();
-// inquirer.prompt(questions).then(({ shape, text, text_color, shape_color }) => {
-//   let logo;
-//   switch (shape) {
-//     case "Circle":
-//       logo = new Circle(text, text_color, shape_color);
-//       break;
-//     case "Square":
-//       logo = new Square(text, text_color, shape_color);
-//       break;
-//     case "Triangle":
-//       logo = new Triangle(text, text_color, shape_color);
-//       break;
-//   }
 
-//function creates the logo.svg file
 function writeFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log("logo.svg made!")
   );
 }
-
-// function genLogo(text, text_color, shape, shape_color) {}
